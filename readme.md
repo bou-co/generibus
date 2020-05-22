df4dd3c36162c3e034fa78f4600c6d201227e6ad
Teemu Lahjalahti <tremppu@hotmail.com>
2020-05-22 14:49:05 +0300

    Repository init

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Generibus
+
+**Work in progress**
+
+A tool for parsing styles from CMS. Generates vanilla CSS that can handle media queries, CSS variables and basic properties.
+
+Examples and NPM package coming soon.
+
+///
+
+Made by [Bou](https://bou.co)
---
+++ src/index.ts
@@ -0,0 +1,103 @@
+export interface StylesProps {
+  styles: string[];
+  className: string;
+  uid: string;
+  children: any;
+}
+
+type StyleTree = { [query: string]: string[] };
+
+export interface MediaQueries {
+  [queryName: string]: string;
+}
+
+const DEFAULT_MEDIA_QUERIES: MediaQueries = {
+  "@all": "@media (min-width: 0)",
+  "@desktop": "@media (min-width: 1200px)",
+  "@tablet": "@media (min-width: 600px) and (max-width: 1200px)",
+  "@mobile": "@media (max-width: 600px), (orientation: portrait)",
+  "@portrait": "@media (orientation: portrait)",
+  "@landscape": "@media (orientation: landscape)",
+};
+
+export function parseStyles(
+  styles: string[],
+  rootClass: string,
+  customQueries: MediaQueries
+): string {
+  if (!styles || styles.length < 1) {
+    return null;
+  }
+  const tree = contructStyleTree(styles);
+  const css = combineToCss(tree, rootClass, customQueries);
+  return css;
+}
+
+function contructStyleTree(styles: string[]): StyleTree {
+  const styleTree = styles.reduce((tree, styleString) => {
+    const [query] = styleString.match(/@.+(?=\()/g);
+    const [selectors] = styleString.match(/(?<=\().+(?=\))/g);
+    if (!query || !selectors) {
+      return tree;
+    }
+    const selectorCssString = constructSelectorCssString(selectors);
+    if (tree[query]) {
+      return { ...tree, [query]: [...tree[query], selectorCssString] };
+    }
+    return { ...tree, [query]: [selectorCssString] };
+  }, {});
+  return styleTree;
+}
+
+function constructSelectorCssString(selectors: string): string {
+  const splittedSelectors = selectors.split("/");
+  // Check if there is selectors
+  if (splittedSelectors.length === 1) {
+    const _css = parseCssValue(selectors);
+    return `{${_css.join(" ")}}`;
+  }
+  return splittedSelectors.reduceRight((tree, selector, i) => {
+    if (splittedSelectors.length === i + 1) {
+      const _css = parseCssValue(selector);
+      return `{${_css.join(" ")}}`;
+    }
+    return `${selector} ${tree}`;
+  }, "");
+}
+
+function parseCssValue(cssString: string): string[] {
+  return cssString
+    .split(";")
+    .filter((item) => !!item)
+    .map((item) => {
+      const [property, value] = item.split(":");
+      // Test if value is a variable
+      if (RegExp(/\$.+/g).test(value)) {
+        const cleaned = value.replace("$", "");
+        return `${property}: var(--${cleaned})`;
+      }
+      return `${property}: ${value};`;
+    });
+}
+
+function combineToCss(
+  styleTree: StyleTree,
+  rootClass: string,
+  customQueries: MediaQueries
+) {
+  const mediaQueries = { ...DEFAULT_MEDIA_QUERIES, ...customQueries };
+  const css = Object.keys(styleTree)
+    .reduce((acc, queryName) => {
+      const queryString = mediaQueries[queryName];
+      if (!queryString) {
+        return acc;
+      }
+      const selectorTree = styleTree[queryName];
+      const cssString = `${queryString} { ${selectorTree
+        .map((selector) => `.${rootClass} ${selector}`)
+        .join(" ")} }`;
+      return [...acc, cssString];
+    }, [])
+    .join("\n");
+  return css;
+}
